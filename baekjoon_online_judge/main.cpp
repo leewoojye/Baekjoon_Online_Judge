c6569a123b93dfaf5719d59e5626dcccd677b010
leewoojye <whitehole36@naver.com>
2024-01-01 13:56:29 +0900

    좋은수구하기

--- baekjoon_online_judge/main.cpp
+++ baekjoon_online_judge/main.cpp
@@ -6,9 +6,49 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int N;
+    cout << "input";
+    cin >> N;
+    vector<int> A(N,0);
+    
+    for (int i =0; i<N; i++) {
+        cin>>A[i];
+    }
+    
+    sort(A.begin(), A.end());
+    int count=0;
+    
+    for (int k=0; k<N; k++) {
+        int find = A[k];
+        int i = 0;
+        int j = N-1;
+        
+        while (i<j) {
+            if (A[i]+A[j]>find) {
+                j--;
+            }
+            else if (A[i]+A[j]<find) {
+                i++;
+            }
+            else {
+                if(i != k && j != k) { // 두 포인터에 해당하는 숫자는 모두 k가 아니어야 함
+                    count++;
+                }
+            }
+        }
+    }
+    
+    cout<<"total: "  << count;
+    
     return 0;
 }
---
+++ baekjoon_online_judge/main2.cpp
@@ -0,0 +1,8 @@
+//
+//  main2.cpp
+//  baekjoon_online_judge
+//
+//  Created by Woo Jye Lee on 1/1/24.
+//
+
+#include <stdio.h>
