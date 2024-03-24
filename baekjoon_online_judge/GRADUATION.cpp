c7f8174a222df1eb8e1e25f0d7588aef6aa073ea
WooJe <whitehole36@naver.com>
2024-03-24 10:15:28 +0000

    const int MAXN

--- baekjoon_online_judge/GRADUATION.cpp
+++ baekjoon_online_judge/GRADUATION.cpp
@@ -2,16 +2,18 @@
 #include <vector>
 #include <algorithm>
 #include <list>
-#include <bit>
-#include <bitset>
-#include <cstdint>
+#include<cstdio>
+#include<cstring>
+#include<string>
 using namespace std;
 
 const int INF = 987654321;
 int n,k,m,l;
+// const int MAXN = 10;
+const int MAXN = 20;
 
 // i번째 과목의 선수과목의 집합
-int prerequisite[MAXN];
+int prerequisite[MAXN]; // MAXN : 전공과목 최대수?
 
 // classes[i] : i번째 학기에 개설되는 과목의 집합, 과목들의 집합을 int형 정수로 표현 가능함.
 // 예. {1,2,3} -> 이진수 1110 -> 십진수 14
@@ -22,6 +24,10 @@ int bitCount(int x) {
     if(x==0) return 0;
     return x%2 + bitCount(x/2);
 }
+
+// int bitCount(int n) {
+// 	return __builtin_popcount(n);
+// }
 
 // 이번 학기가 semester고, 지금까지 들은 과목의 집합이 taken일 때, k개 이상의 과목을 모두 들으려고 할 때 필요한 학기 수를 반환.
 int graduate(int semester, int taken) {
@@ -34,7 +40,7 @@ int graduate(int semester, int taken) {
     ret = INF;
 
     // 이번 학기에 들을 수 있는 과목 중 아직 듣지 않은 과목들을 찾음.
-    int canTake = (classes[semester & ~taken]);
+    int canTake = (classes[semester] & ~taken);
 
     // 선수 과목을 다 듣지 않은 과목들을 걸러냄.
     for (int i = 0; i<n; ++i) {
@@ -53,5 +59,33 @@ int graduate(int semester, int taken) {
 }
 
 int main() {
-    return 0;
+	int cases;
+	cin >> cases;
+	for(int cc = 0; cc < cases; ++cc) {
+		cin >> n >> k >> m >> l;
+		for(int i = 0; i < n; i++) {
+			prerequisite[i] = 0;
+			int r;
+			cin >> r;
+			for(int j = 0; j < r; j++) {
+				int required;
+				cin >> required;
+				prerequisite[i] |= (1 << required);
+			}
+		}
+		for(int i = 0; i < m; i++) {
+			classes[i] = 0;
+			int c;
+			cin >> c;
+			for(int j = 0; j < c; j++) {
+				int open;
+				cin >> open;
+				classes[i] |= (1 << open);
+			}
+		}
+		memset(cache, -1, sizeof(cache));
+		int ret = graduate(0, 0);
+		if(ret >= 987654321) ret = -1;
+		cout << ret << endl;
+	}
 }
