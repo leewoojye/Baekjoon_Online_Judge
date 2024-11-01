bedb378aa9de4fd7ba68b400d39d35aeb60dc925
WOOJYE <whitehole36@naver.com>
2024-11-01 11:09:17 +0000

    1977

--- baekjoon_online_judge/1977.cpp
+++ baekjoon_online_judge/1977.cpp
@@ -5,11 +5,32 @@ using namespace std;
 int arr[101];
 
 int main() {
-  // for(int i=0;i<101;++i) {
-  //   arr[i]=i*i;
-  // }
-  int from, to;
-  cin >> from >> to;
-
+  fastio;
+  arr[0]=0;
+  for(int i=1;i<101;++i) {
+    arr[i]=i*i;
+  }
+  int M, N;
+  cin >> M;
+  cin >> N;
+  int p1=1, p2=100;
+  while(arr[p1]<M || arr[p2]>N) {
+    if(arr[p1]<M) {
+      p1++;
+    }
+    if(arr[p2]>N) {
+      p2--;
+    }
+  }
+  int sum=0;
+  for(int i=p1;i<=p2;++i) {
+    sum+=arr[i];
+  }
+  if(sum==0) {
+    cout << -1 << '\n';
+  } else {
+    cout << sum << '\n';
+    cout << arr[p1];
+  }
   return 0;
 }
--- baekjoon_online_judge/4948.cpp
+++ baekjoon_online_judge/4948.cpp
@@ -7,6 +7,7 @@ using namespace std;
 int findPrime(int n) {
   int start;
   int count=0;
+  // 입력이 2인 특별한 경우를 처리함(유일한 짝수 소수)
   if(n==1) {
     return 1;
   }
@@ -16,6 +17,7 @@ int findPrime(int n) {
     start=n+2;
   }
   for(int i=start;i<2*n;i+=2) {
+    // 입력값=2~8인 경우 루트값이 3보다 작기 때문에 소수 후보 j는 1부터 시작해야함
     for(int j=1;j<=(int)sqrt(i);j+=2) {
       if(i%j==0 && j>1) {
         break;
