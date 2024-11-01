09b9279d9bbb85d2721b49cfd80814c31653092b
WOOJYE <whitehole36@naver.com>
2024-11-01 10:26:38 +0000

    4948

---
+++ baekjoon_online_judge/tempCodeRunnerFile.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+#define fastio ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+int findPrime(int n) {
+  int start;
+  int count=0;
+  if(n==1) {
+    return 1;
+  }
+  if(n%2==0) {
+    start=n+1;
+  } else {
+    start=n+2;
+  }
+  for(int i=start;i<2*n;i+=2) {
+    for(int j=1;j<=(int)sqrt(i);j+=2) {
+      if(i%j==0 && n>=9) {
+        break;
+      } else if (j==(int)sqrt(i) || j==(int)sqrt(i)-1) {
+        count++;
+      }
+    }
+  }
+  return count;
+}
+
+int main() {
+  fastio;
+  vector<int> input;
+  int tmp;
+  while(1) {
+    cin >> tmp;
+    if(tmp==0) {
+      break;
+    }
+    input.push_back(tmp);
+  }
+  for(auto& n : input) {
+    cout << findPrime(n) << '\n';
+  }
+  return 0;
+}
