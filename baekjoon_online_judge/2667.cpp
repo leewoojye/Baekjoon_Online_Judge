1111614f16f6aadc24fa982804ef96e37e1c2a6c
WOOJYE <whitehole36@naver.com>
2024-11-05 11:51:08 +0000

    29_7

--- baekjoon_online_judge/2667.cpp
+++ baekjoon_online_judge/2667.cpp
@@ -7,7 +7,9 @@ using namespace std;
 
 vector<vector<int>> matrix;
 vector<vector<bool>> visited;
+// 각 구역의 결과를 저장하는 벡터
 vector<int> result;
+// 구역번호를 저장하는 변수 indexx
 int indexx=0;
 int dx[4]={0,1,-1,0};
 int dy[4]={1,0,0,-1};
@@ -17,6 +19,7 @@ void searchDFS(int x, int y) {
         int newx=x+dx[i];
         int newy=y+dy[i];
         if(newx<0 || newx>matrix.size()-1 || newy<0 || newy>matrix.size()-1 || visited[newx][newy]) continue;
+        // 주변 칸 중 1인 칸에 대해서만 dfs하여 한번에 하나씩 구역을 찾음
         if(matrix[newx][newy]==1) {
             result[indexx]++;
             visited[newx][newy]=true;
@@ -37,9 +40,11 @@ int main()
     for(int i=0;i<N;++i) {
         cin >> s;
         for(int j=0;j<N;++j) {
+            // 숫자문자를 형변환하면 아스키코드로 대체되므로 문자0을 먼저 빼준 후 형변환
             matrix[i][j]=(int)(s[j]-'0');
         }
     }
+    // 첫원소는 미리 넣어주고 시작
     result.push_back(0);
     for(int x=0;x<N;++x) {
         for(int y=0;y<N;++y) {
@@ -53,6 +58,7 @@ int main()
         }
     }
     sort(result.begin(), result.end());
+    // dfs 탐색이 끝날때마다 0원소를 넣어주므로 값이0인 원소는 지워준다 (답에서 제외)
     result.erase(result.begin());
     cout << result.size() << '\n';
     for(auto& o : result) {
