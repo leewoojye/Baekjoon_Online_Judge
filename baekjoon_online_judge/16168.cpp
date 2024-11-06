6fb4c63ed87aababd45e61e52fb794c2552eb8fb
WOOJYE <whitehole36@naver.com>
2024-11-06 11:47:02 +0000

    16168

---
+++ baekjoon_online_judge/16168.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+#define fastio ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+int V,E;
+vector<vector<int>> matrix;
+bool isEulerTrail(int here, int count) {
+    for(int i=1;i<=V;++i) {
+        if(matrix[here][i]>0) {
+            matrix[here][i]--;
+            matrix[i][here]--;
+            if(isEulerTrail(i, count++)) return true;
+            matrix[here][i]++;
+            matrix[i][here]++;
+        }
+    }
+    if(count==E) return true;
+    return false;
+}
+
+int main()
+{
+    fastio;
+    int e1,e2;
+    cin >> V >> E;
+    matrix.resize(V+1, vector<int>(V+1, 0));
+    for(int i=0;i<E;++i) {
+        cin >> e1 >> e2;
+        matrix[e1][e2]++;
+        matrix[e2][e1]++;
+    }
+    for(int i=1;i<=V;++i) {
+        int edges=0;
+        for(int j=1;j<=V;++j) {
+            edges+=matrix[i][j];
+        }
+        if(edges%2 != 0) {
+            string s;
+            isEulerTrail(i, 0) ? s="YES" : s="NO";
+            cout << s;
+            return 0;
+        }
+    }
+    cout << "NO";
+    return 0;
+}
