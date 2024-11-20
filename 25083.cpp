deb9927e7635fed584d9d1af7e93895ca0558cb6
leewoojye <whitehole36@naver.com>
2024-11-21 01:32:05 +0900

    25083

---
+++ 25083.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+#define fastio ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+int main() {
+  fastio;
+  string s="         ,r'\"7\nr`-_   ,'  ,/\n \\. \". L_r'\n   `~\\/\n      |\n      |";
+  cout << s;
+  return 0;
+}
