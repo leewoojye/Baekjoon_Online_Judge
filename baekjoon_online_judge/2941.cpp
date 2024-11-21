bafa1165d9cd5dcc2c14f0f464d6a3ffa4286281
leewoojye <whitehole36@naver.com>
2024-11-21 09:47:19 +0900

    2941

---
+++ baekjoon_online_judge/2941.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+#define fastio ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+int main() {
+  fastio;
+  string s;
+  cin >> s;
+  int index=0, count=0;
+  while(s[index]!='\0') [
+    if(s[index]=='=' || s[index]=='-' || s[index])
+  ]
+  return 0;
+}
