a463bf435e980a466495e37693734e1c1e256800
WOOJYE <whitehole36@naver.com>
2024-11-23 02:53:43 +0000

    11723

--- baekjoon_online_judge/11723a.cpp
+++ baekjoon_online_judge/11723a.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 #include <string>
-#include <bitset>
-#include <vector>
 using namespace std;
+
 #define fastio ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
 enum Command {
@@ -25,58 +24,52 @@ Command stringToCommand(const string& str) {
     return CMD_UNKNOWN;
 }
 
-int main()
-{
+int main() {
     fastio;
     int M;
     string cmd;
-    int S(0);
-    int S0(0);
+    int S = 0;
     int x;
-    vector<bitset<1>> result;
+    
     cin >> M;
-    for(int i=0;i<M;++i) {
-        cmd.clear();
+    for (int i = 0; i < M; ++i) {
         cin >> cmd;
         
-        if(cmd=="all") {
-            S |= ((1<<20)-1);
+        if (cmd == "all") {
+            S |= ((1 << 20) - 1);  // 모든 비트 1로 설정
             continue;
         }
-        if(cmd=="empty") {
-            S=0;
+        if (cmd == "empty") {
+            S = 0;  // 모든 비트 0으로 설정
             continue;
         }
+        
         cin >> x;
-        x-=1;
-        Command command=stringToCommand(cmd);
-        switch(command) {
+        x -= 1;
+        Command command = stringToCommand(cmd);
+        
+        switch (command) {
             case CMD_ADD:
-            S |= (1<<x);
-            break;
+                S |= (1 << x);
+                break;
             case CMD_REMOVE:
-            // S0=(S>>x)-1;
-            // S0=(S0<<x);
-            // S=S0 | S;
-            S = S & ~(1 << x);
-            break;
+                S = S & ~(1 << x);
+                break;
             case CMD_CHECK:
-            if(((1<<x) & S) != 0) {
-                result.push_back(1);
-            } else {
-                result.push_back(0);
-            }
-            break;
+                // check 결과를 즉시 출력
+                // 입력값 다 받고 마지막에 출력됨 how?
+                if ((S & (1 << x)) != 0) {
+                    cout << 1 << '\n';
+                } else {
+                    cout << 0 << '\n';
+                }
+                break;
             case CMD_TOGGLE:
-            S ^= (1 << x);
-            //S ^= 0;
-            break;
+                S ^= (1 << x);
+                break;
             default:
-            break;
+                break;
         }
-    }
-    for(int i=0;i<result.size();++i) {
-        cout << result[i] << '\n';
     }
     return 0;
 }
