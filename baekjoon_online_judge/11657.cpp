fb0f148b259aed8891b778a68e1faf11384b5f2c
WOOJYE <whitehole36@naver.com>
2024-12-05 10:56:31 +0000

    11657

--- baekjoon_online_judge/11657.cpp
+++ baekjoon_online_judge/11657.cpp
@@ -9,20 +9,24 @@ vector<vector<pair<int,int>>> list;
 vector<int> upper;
 int N,M;
 
-void belmanford(int start) {
+int belmanford(int start) {
   bool updated;
+  upper[start]=0;
   for(int i=0;i<N;++i) {
     updated=false;
     for(int j=1;j<=N;++j) {
-      int here=
       for(int k=0;k<list[j].size();++k) {
         int adj=list[j][k].first;
         int cost=list[j][k].second;
-        if(upper[<upper[adj])
+        if(upper[j]+cost<upper[adj]) {
+          upper[adj]=upper[j]+cost;
+          updated=true;
+        }
       }
     }
   }
-  return;
+  if(updated) return -1;
+  return 0;
 }
 
 int main() {
@@ -30,10 +34,22 @@ int main() {
   int v1,v2,w;
   cin >> N >> M;
   list.resize(N+1);
-  upper.resize(N+1);
+  upper.resize(N+1, INT_MAX-1000);
   for(int i=0;i<M;++i) {
     cin >> v1 >> v2 >> w;
     list[v1].push_back({v2,w});
+  }
+  int result=belmanford(1);
+  if(result==-1) {
+    cout << -1;
+    return 0;
+  }
+  for(int i=2;i<=N;++i) {
+    if(upper[i]==INT_MAX-1000) {
+      cout << -1 << '\n';
+      continue;
+    }
+    cout << upper[i] << '\n';
   }
   return 0;
 }
