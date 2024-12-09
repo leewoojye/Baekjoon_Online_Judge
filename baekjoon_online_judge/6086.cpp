f8f8424ae77ffafc91b02bcca818fe77a36c225b
WOOJYE <whitehole36@naver.com>
2024-12-09 10:55:01 +0000

    6086

--- baekjoon_online_judge/6086.cpp
+++ baekjoon_online_judge/6086.cpp
@@ -12,12 +12,14 @@ int main() {
   cin >> E;
   // 잔여용량이랑 유량 같은 배열로 쓰기
   vector<vector<int>> flow(26, vector<int>(26, INT_MAX));
+  // flow[0][0]=0;
+
   char c1,c2;
   int f;
   for(int i=0;i<E;++i) {
     cin >> c1 >> c2 >> f;
     flow[c1-65][c2-65]=f;
-    flow[c2-65][c1-65]=f;
+    // flow[c2-65][c1-65]=f;
   }
   int ret=0;
   while(true) {
@@ -25,20 +27,22 @@ int main() {
     parent[0]=0;
     queue<int> q;
     q.push(0);
-    int amount;
+    int amount=0;
     while(!q.empty()) {
       int front=q.front(); q.pop();
       int minNode=0;
       for(int i=0;i<26;++i) {
-        if(flow[front][i]>0 && flow[front][i]<flow[front][minNode]) minNode=i;
-        amount=flow[front][i];
+        if(flow[front][i]!=INT_MAX && flow[front][i]<flow[front][minNode]) {
+          minNode=i;
+          amount=flow[front][i];
+        }
       }
       parent[minNode]=front;
       if(parent[25]!=-1) break;
     }
     if(parent[25]==-1) break;
     for(int p=25;parent[p]!=0;p=parent[p]) {
-      flow[p][parent[p]]-=amount;
+      // flow[p][parent[p]]-=amount;
       flow[parent[p]][p]-=amount;
     }
     ret+=amount;
