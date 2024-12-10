12dabcc07da51b2cfa6a133de774f8da6ff321ef
WOOJYE <whitehole36@naver.com>
2024-12-10 14:02:24 +0000

    6086

--- baekjoon_online_judge/6086.cpp
+++ baekjoon_online_judge/6086.cpp
@@ -23,35 +23,29 @@ int main() {
   }
   int ret=0;
   while(true) {
-    
     vector<bool> visited(26, false);
     vector<int> parent(26, -1);
     parent[0]=0;
     visited[0]=true;
     queue<int> q;
     q.push(0);
-    int amount=0;
-
-    while(!q.empty()) {
-
+    int amount=INT_MAX;
+    while(!q.empty() && parent[25]==-1) {
       int front=q.front(); q.pop();
       visited[front]=true;
       int minNode=0;
-
+      // BFS
       for(int i=0;i<26;++i) {
-        if(!visited[i] && flow[front][i]<flow[front][minNode]) {
-          minNode=i;
-          amount=flow[front][i];
+        if(!visited[i] && flow[front][i]<INT_MAX && flow[front][i]>0) {
+          if(flow[front][i]<flow[front][minNode]) minNode=i; 
+          amount=min(amount,flow[front][i]);
+          parent[i]=front;
+          q.push(i);
         }
       }
-      parent[minNode]=front;
-      // if(parent[25]!=-1) break;
-      amount=0;
     }
-
     if(parent[25]==-1) break;
-
-    for(int p=25;parent[p]!=0;p=parent[p]) {
+    for(int p=25;p!=0;p=parent[p]) {
       flow[p][parent[p]]-=amount;
       flow[parent[p]][p]-=amount;
     }
