4761b768fdab7024f6cc897e38d0d60d33cf71a5
WOOJYE <whitehole36@naver.com>
2024-12-15 03:44:37 +0000

    comment

--- baekjoon_online_judge/5052.cpp
+++ baekjoon_online_judge/5052.cpp
@@ -7,9 +7,9 @@ using namespace std;
 struct TrieNode {
   vector<TrieNode*> children;
   bool terminal;
-  bool hasChild;
+  bool hasChild; // 현재 노드가 자식이 있는지 여부를 저장. 자식이 있으면 어떤 문자열의 접두어이므로 문제에서 NO를 반환해야한다
   TrieNode() {
-    // 함수가 아닌 구조체 내부에서는 생성자 안에서 초기화작업을 해줌
+    // 함수가 아닌 구조체 내부에서는 생성자 안에서 초기화작업을 해준다
     children.resize(10, nullptr);
     this->terminal=false;
     this->hasChild=false;
@@ -21,7 +21,7 @@ struct TrieNode {
     return children[*key-'0']->find(key+1);
   }
   // insert() : 인자로 주어진 문자열을 트라이에 삽입
-  // 주어진 문자를 현재 노드의 자식으로 집어넣는다
+  // 주어진 문자 key를 현재 노드의 맴버로(구체적으론 자식을 나타내는 배열) 집어넣는다
   void insert(const char* key) {
     if(*key==0) { this->terminal=true; return; }
     if(this->children[*key-'0']==nullptr) {
@@ -47,7 +47,7 @@ int main() {
     TrieNode* node=new TrieNode();
     for(int j=0;j<n;++j) {
       cin >> s;
-      list[j]=s;
+      list[j]=s; // 문자열 s를 '복사'전달하였으니 insert() 인자로는 s가 아닌 list의 주소를 전달한다
       node->insert(&list[j][0]);
     }
     bool flag=true;
