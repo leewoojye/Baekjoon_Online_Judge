e602d5507fb50fcc37b709422aae2c60d69edcbc
WOOJYE <whitehole36@naver.com>
2024-12-29 09:32:32 +0000

    2098

--- baekjoon_online_judge/2098.cpp
+++ baekjoon_online_judge/2098.cpp
@@ -8,17 +8,22 @@ using namespace std;
 vector<vector<int>> matrix;
 int start;
 int N;
+short fullvisited;
 // visited 목록의 도시를 거쳐 here까지 왔을 때 시작점부터 here를 포함한 경로의 최소비용을 반환
 int searchTSP(short visited, int here) {
   // here 방문처리
   visited |= (1 << here);
-  if(visited==-1 && here==start) return 0;
-  if(visited==-1) return INT_MAX;
-  int ret=INT_MAX;
+  if((visited==fullvisited) && (here==start)) {
+    return 0;
+  }
+  if(visited==fullvisited) {
+    return 987654321;
+  }
+  int ret=987654321;
   for(int i=0;i<N;++i) {
     int cost=matrix[here][i];
     // 아직 방문하지 않은 도시면 방문
-    if(!(visited & (1 << i))) {
+    if((!(visited & (1 << i)) || (i==start)) && here!=i) {
       ret=min(ret,searchTSP(visited,i)+cost);
     }
   }
@@ -29,6 +34,7 @@ int main() {
   fastio;
   cin >> N;
   matrix.resize(N);
+  fullvisited = (1 << N) - 1;
   int w;
   for(int i=0;i<N;++i) {
     for(int j=0;j<N;++j) {
@@ -36,7 +42,7 @@ int main() {
       matrix[i].push_back(w);
     }
   }
-  int result=INT_MAX;
+  int result=987654321;
   for(int i=0;i<N;++i) {
     start=i;
     result=min(result,searchTSP(0,i));
