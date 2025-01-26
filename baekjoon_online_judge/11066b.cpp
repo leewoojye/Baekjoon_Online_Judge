f30711cfcbde2fca19127cce29c1b75430592224
WOOJYE <whitehole36@naver.com>
2025-01-26 10:16:37 +0000

    12865

--- baekjoon_online_judge/11066b.cpp
+++ baekjoon_online_judge/11066b.cpp
@@ -7,20 +7,16 @@ using namespace std;
 int main() {
     int T;
     cin >> T;
-
     while (T--) {
         int N;
         cin >> N;
-
         vector<int> files(N + 1);  // 파일 크기
         vector<vector<int>> dp(N + 1, vector<int>(N + 1, 0));
         vector<vector<int>> sum(N + 1, vector<int>(N + 1, 0));
-
         // 파일 크기 입력
         for (int i = 1; i <= N; i++) {
             cin >> files[i];
         }
-
         // 누적합 계산
         for (int i = 1; i <= N; i++) {
             sum[i][i] = files[i];
@@ -28,7 +24,6 @@ int main() {
                 sum[i][j] = sum[i][j - 1] + files[j];
             }
         }
-
         // DP 테이블을 채우기
         for (int length = 2; length <= N; length++) {
             for (int i = 1; i <= N - length + 1; i++) {
@@ -39,10 +34,8 @@ int main() {
                 }
             }
         }
-
         // 최소 비용 출력
         cout << dp[1][N] << endl;
     }
-
     return 0;
 }
