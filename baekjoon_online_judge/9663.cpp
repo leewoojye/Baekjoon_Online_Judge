d3ce674e42677e8f2f7012395f7c11f5f2545ffe
leewoojye <whitehole36@naver.com>
2025-07-11 23:52:29 +0900

    9663

---
+++ baekjoon_online_judge/9663.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+#define fastio ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+int main() {
+  fastio;
+  int N;
+  cin >> N;
+  
+  return 0;
+}
