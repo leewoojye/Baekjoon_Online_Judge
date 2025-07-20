6fab54b2bff98bc90cc7b80c4291039d9d0d67ac
WOOJYE <whitehole36@naver.com>
2025-07-20 14:34:49 +0000

    4153

--- baekjoon_online_judge/4153.cpp
+++ baekjoon_online_judge/4153.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 #define fastio ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 using namespace std;
 
@@ -14,9 +15,10 @@ int main() {
     int pow1=0,pow2=0;
     for(int i=0;i<3;++i) {
       cin >> lengths[i];
-      if(lengths[i]==0) break;
-      if(i==0) maxlength=lengths[0];
-      if(i!=0 && lengths[i]>lengths[i-1]) maxlength=lengths[i];
+      if(lengths[0]==0) break;
+      if(i==0) { maxlength=lengths[0]; continue; }
+      // if(i!=0 && lengths[i]>lengths[i-1]) maxlength=lengths[i];
+      maxlength=max(lengths[i],maxlength); // 윗줄 이걸로 바꿨더니 맞음.. 뭐임?
     }
     if(lengths[0]==0) break;
     pow1=maxlength*maxlength;
