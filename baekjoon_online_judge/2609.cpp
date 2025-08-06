0ab27aa19867186275aa47a7844081ff33ebdf13
leewoojye <whitehole36@naver.com>
2025-08-06 17:46:16 +0900

    2609

---
+++ baekjoon_online_judge/2609.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+#define fastio ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+int main() {
+  fastio;
+  int n1,n2;
+  cin >> n1 >> n2;
+  int result1,result2;
+  int c1=n1, c2=n2;
+  int smaller=min(c1,c2);
+  int bigger=max(c1,c2);
+  for(int i=smaller;i>0;--i) {
+    if(c1%i==0 && c2%i==0) {result1=i; break;}
+  }
+  bool flag=false;
+  for(int i=1;i<=smaller;++i) {
+    for(int j=1;j*smaller<=bigger*i;++j) {
+      if(j*smaller==bigger*i) {result2=j*smaller;flag=true;break;}
+    }
+    if(flag) break;
+  }
+  cout << result1 << '\n' << result2 << '\n';
+  return 0;
+}
