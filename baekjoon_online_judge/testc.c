64740b9116f92e2ff244c55c5171262e70025c0c
leewoojye <whitehole36@naver.com>
2025-10-04 23:52:33 +0900

    comment

--- baekjoon_online_judge/testc.c
+++ baekjoon_online_judge/testc.c
@@ -1,30 +1,21 @@
 #include <stdio.h>
-#define SIZE 10
+#include <string.h>
 
-int main()
-{
-    int a[SIZE] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, even[SIZE], odd[SIZE], i, j, k;
+#define MAX 100
 
-    // a[i], even[j], odd[k]
-    for (i = j = k = 0; i < SIZE; i++) {
-        if (a[i] % 2 == 0) // if even, store in even array
-            even[j++] = a[i];
-        else // if odd, store in odd array
-            odd[k++] = a[i]; // 채워야 할 부분
+int main() {
+    char s1[] = "LEE WOOJYE"; 
+    char s2[MAX];
+    int len = strlen(s1);
+    int i, j;
+
+    for (i = len - 1, j = 0; i >= 0; i--, j++) {
+        s2[j] = s1[i];
     }
+    s2[len] = '\0'; 
 
-    // print even array
-    printf("even = ");
-    for (i = 0; i < j; i++) { // 채워야 할 부분
-        printf("%d ", even[i]);
-    }
+    printf("Before: %s\n", s1);
+    printf("After: %s\n", s2);
 
-    // print odd array
-    printf("\n");
-    printf("odd = ");
-    for (i = 0; i < k; i++) {
-        printf("%d ", odd[i]);
-    }
-    printf("\n");
     return 0;
 }
