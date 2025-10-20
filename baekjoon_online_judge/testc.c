6d885b97244c36d263347c9a6597b24823cf6cf4
leewoojye <whitehole36@naver.com>
2025-10-20 19:08:48 +0900

    1085

--- baekjoon_online_judge/testc.c
+++ baekjoon_online_judge/testc.c
@@ -1,30 +1,47 @@
 #include <stdio.h>
-#define SIZE 5
 
-int data[SIZE] = {0, 10, 20, 30, 40};
-// 아래 4줄 완성
-int next[SIZE] = {-1, 3, 1, 0, 2};
-int prev[SIZE] = {3, 2, 4, 1, -1};
-int head = 4;
-int last = 0;
+#define SIZE 8 
 
-void PrintList()
-{
-    int i;
-    printf("(head = %d) => ", head);
-    for (i = head; i != -1; i = next[i])
-    {
-        printf("%d (next = %d) => ", data[i], next[i]);
-    }
-    printf("(null)\n(last = %d) => ", last);
-    for (i = last; i != -1; i = prev[i]) {
-        printf("%d (prev = %d) => ", data[i], prev[i]);
-    }
-    printf("(null)\n");
-}
+void Merge(int a[], int b[], int c[], int length); 
+void PrintArray(int a[], int length) { 
+    int i; 
+    for (i = 0; i < length; i++) printf("%d ", a[i]); 
+    printf("\n"); 
+} 
 
-int main()
-{
-    PrintList();
-    return 0;
+int main() { 
+    int a[SIZE] = {5, 3, 6, 1, 7, 2, 0, 4}, b[SIZE]; 
+    PrintArray(a, SIZE); 
+    
+    // length 1 --> 2 
+    Merge(b + 0, a + 0, a + 1, 1); 
+    Merge(b + 2, a + 2, a + 3, 1); 
+    Merge(b + 4, a + 4, a + 5, 1); 
+    Merge(b + 6, a + 6, a + 7, 1); // 원본 코드 에 누락된 부분
+    PrintArray(b, SIZE); 
+    
+    // length 2 --> 4 
+    Merge(a + 0, b + 0, b + 2, 2); 
+    Merge(a + 4, b + 4, b + 6, 2); // 원본 코드 에 누락된 부분
+    PrintArray(a, SIZE); 
+    
+    // length 4 --> 8 
+    Merge(b + 0, a + 0, a + 4, 4); 
+    PrintArray(b, SIZE); 
+    
+    return 0; 
+} 
+
+void Merge(int a[], int b[], int c[], int length) { // Merge two sorted lists 
+    int i, j, k; 
+    for (i = j = k = 0; i < 2 * length; i++) { 
+        if (j == length) 
+            a[i] = c[k++]; 
+        else if (k == length) 
+            a[i] = b[j++]; // 빈 칸 채우기
+        else if (b[j] > c[k]) // 내림차순 (descending) 
+            a[i] = b[j++]; 
+        else 
+            a[i] = c[k++]; 
+    } 
 }
--- baekjoon_online_judge/testim.cpp
+++ baekjoon_online_judge/testim.cpp
@@ -1,37 +1,50 @@
 #include <iostream>
+using namespace std;
 
-class Count {
-private:
-    int num;
-
+class Vehicle {
 public:
-    Count(int n = 0) {
-        num = n;
-        std::cout << "기본 생성자 호출 (num = " << num << ")" << std::endl;
-    }
+    virtual void Type() {}
+    virtual void Usage() { cout << "Transportation, "; }
+    // virtual 제거
+    void Wheel() { cout << "4 wheels, "; } 
+    // virtual 제거
+    void Door() { cout << "No door" << endl; } 
+    void Print() { Type(); Usage(); Wheel(); Door(); } 
+};
 
-    Count(int n1, int n2) {
-        num = (n1 > n2) ? n1 : n2; 
-        std::cout << "오버로딩된 생성자 호출 (num = " << num << ")" << std::endl;
-    }
+class Motorcycle: public Vehicle {
+public:
+    void Type() { cout << "Motorcycle: "; }
+    void Wheel() { cout << "2 wheels, "; } // 
+};
 
-    void Print() {
-        std::cout << "현재 num 값: " << num << std::endl;
-    }
+class Car: public Vehicle {
+public:
+    void Door() { cout << "4 doors" << endl; } // 
+};
+
+class Sedan: public Car {
+public:
+    void Type() { cout << "Sedan: "; }
+};
+
+class Coupe: public Car {
+public:
+    void Type() { cout << "Coupe: "; }
+    void Door() { cout << "2 doors" << endl; } // 
+};
+
+class Hatchback: public Car {
+public:
+    void Type() { cout << "Hatchback: "; }
+    void Door() { cout << "5 doors" << endl; } // 
 };
 
 int main() {
-    Count c1;
-    c1.Print();
-
-    Count c2(10);
-    c2.Print();
-
-    Count c3(20, 30); 
-    c3.Print();
-
-    Count c4(100, 50); 
-    c4.Print();
-
+    Motorcycle m;
+    Sedan s;
+    Coupe c;
+    Hatchback h;
+    m.Print(); s.Print(); c.Print(); h.Print(); 
     return 0;
 }
