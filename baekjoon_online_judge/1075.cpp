a8e6082fdda9da49c125ee62cb23d33f3225ed88
leewoojye <whitehole36@naver.com>
2025-10-30 22:02:28 +0900

    1075

---
+++ baekjoon_online_judge/1075.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N, F;
+    cin >> N >> F;
+    N = N - (N % 100);
+    for (int i = 0; i < 100; i++) {
+        if ((N + i) % F == 0) {
+            if (i < 10) cout << "0";
+            cout << i << endl;
+            break;
+        }
+    }
+    return 0;
+}
